27d8ff7c53074180d11e60ff1d6818ff6dcbc22a
SipSaver <robertmckenna05@outlook.com>
2025-08-23 16:25:15 +1000

    Revert "test: add jest and pytest setups"

--- frontend/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'jest-expo',
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native|expo(nent)?|@expo(nent)?/.*|@expo/.*|expo(nent)?|@unimodules|unimodules|sentry-expo|native-base|@sentry/.*)'
-  ]
-};
